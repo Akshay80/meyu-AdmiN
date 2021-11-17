5fc013bf46d3ecc3e19776d6c6a45b58775bce69
Akshay80 <akshaybisht80@gmail.com>
2021-11-17 11:43:57 +0530

    17-11-2021

--- src/Components/Auth/Login/Login.js
+++ src/Components/Auth/Login/Login.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.scss'
 import Path from '../../../Constant/RouterConstant'
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+  const [email, setEmail] = useState();
+  const { handleSubmit, register, errors } = useForm();
+  function login() 
+{
+  alert("Data Registered!");
+    } 
     return (
         <div className="container align-items-center">
         <div className="row text-center justify-content-center">
@@ -11,9 +18,17 @@ const Login = () => {
   <div className="card-body">
     <h3 className="card-title1">MEYU</h3>
     <p className="card-title2 mb-5">Meyu Admin</p>
+    <form onSubmit={handleSubmit(login)}>
     <div className="mb-3 row justify-content-center">
     <div className="col-sm-10">
-  <input type="email" className="form-control shadow-none" id="exampleFormControlInput1" placeholder="Email" />
+  <input type="email" className="form-control shadow-none" id="email"
+              onChange={(e) => setEmail(e.target.value)}
+              defaultValue={email}
+              error={!!errors.email}
+              inputRef={register({ required: true,
+                pattern: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+              })} placeholder="Email" />
+               <p className="errors">{errors.email && "Invalid email address"}</p>
 </div>
 </div>
 <div className="mb-3 row justify-content-center">
@@ -27,8 +42,11 @@ const Login = () => {
       </div>
       </div>
     <p>Forgot Password? <a href={Path.forgotPassword} className="link">Click Here</a></p>
+    </form>
   </div>
+
 </div>
+
             
 
 
