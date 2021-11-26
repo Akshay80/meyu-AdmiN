e38f663e2e4662ddb8acd525289db6bfc621f23b
Mohammad Ahmed <mohd.ahmed379@gmail.com>
2021-11-26 09:31:21 +0530

    api

--- src/Components/Auth/Login/Login.js
+++ src/Components/Auth/Login/Login.js
@@ -1,11 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Login.scss";
 import Path from "../../../Constant/RouterConstant";
 import { useForm } from "react-hook-form";
+import axiosConfig from "../../Common/APIConfig/axiosConfig";
+import { ToastContainer, toast, Flip } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
+
+let toastId = null;
 
 const Login = () => {
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
+  // const [email, setEmail] = useState();
+  // const [password, setPassword] = useState();
   const {
     register,
     handleSubmit,
@@ -13,84 +18,122 @@ const Login = () => {
   } = useForm();
 
   function login1(data) {
-    alert("Data Registered!");
-    setEmail(data.email);
-    setPassword(data.password);
-    console.log("Email:", email);
-    console.log("Password: ", password);
+    const loginData = {
+      username: data.email,
+      password: data.password,
+    };
+    axiosConfig
+      .post("/authenticateadmin", loginData)
+      .then(function (response) {
+        console.log(response);
+        console.log(response.status);
+        if (response.data.success === true) {
+          localStorage.setItem("token", response?.data?.token);
+        }
+
+        if (response.data.success === false && !toast.isActive(toastId)) {
+          toast.error(response.data.error.message, {
+            position: "bottom-center",
+            autoClose: 5000,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: false,
+            progress: 0,
+            toastId: "my_toast",
+          });
+        }
+      })
+
+      .catch(function (error) {
+        console.log(error);
+      });
   }
   return (
-    <div className="auth-wrapper align-items-center bg-dark">
-      <div className="row text-center justify-content-center">
-        <div className="cards1 mb-5 align-middle">
-          <div className="card-body">
-            <h3 className="card-title1">MEYU</h3>
-            <p className="card-title2 mb-4">Meyu Admin</p>
-            <p className="card-title3">Sign In</p>
-            <p className="card-subtitle mb-4">
-              Don't have an account?{" "}
-              <a href={Path.signup} className="link">
-                Sign up
-              </a>
-            </p>
-            <form autoComplete="off" onSubmit={handleSubmit(login1)}>
-              <div className="mb-3 row justify-content-center">
-                <div className="col-sm-12">
-                  <input
-                    type="email"
-                    className="form-control shadow-none"
-                    name="email"
-                    onChange={(e) => setEmail(e.target.value)}
-                    placeholder="Email"
-                    {...register("email", {
-                      required: "Email is required",
-                      pattern: {
-                        value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                        message: "Invalid Email",
-                      },
-                    })}
-                  />
-                  {errors.email && (
-                    <p className="errors">{errors.email.message}</p>
-                  )}
-                </div>
-              </div>
-              <div className="mb-3 row justify-content-center">
-                <div className="col-sm-12">
-                  <input
-                    name="password"
-                    placeholder="Password"
-                    className="form-control shadow-none"
-                    type="password"
-                    onChange={(e) => setPassword(e.target.value)}
-                    {...register("password", {
-                      required: "Password is required",
-                    })}
-                  />
-                  {errors.password && (
-                    <p className="errors">{errors.password.message}</p>
-                  )}
-                </div>
-              </div>
-              <div className="mt-4 mb-4 row justify-content-center">
-                <div className="col-sm-12">
-                  <button type="submit" className="btn btn-auth">
-                    Sign In
-                  </button>
-                </div>
-              </div>
-
-              <p>
-                Forgot Password?{" "}
-                <a href={Path.forgotPassword} className="link">
-                  Click Here
+    <>
+      <div className="auth-wrapper align-items-center bg-dark">
+        <div className="row text-center justify-content-center">
+          <div className="cards1 mb-5 align-middle">
+            <div className="card-body">
+              <h3 className="card-title1">MEYU</h3>
+              <p className="card-title2 mb-4">Meyu Admin</p>
+              <p className="card-title3">Sign In</p>
+              <p className="card-subtitle mb-4">
+                Don't have an account?{" "}
+                <a href={Path.signup} className="link">
+                  Sign up
                 </a>
               </p>
-            </form>
+              <form autoComplete="off" onSubmit={handleSubmit(login1)}>
+                <div className="mb-3 row justify-content-center">
+                  <div className="col-sm-12">
+                    <input
+                      type="email"
+                      className="form-control shadow-none"
+                      name="email"
+                      placeholder="Email"
+                      {...register("email", {
+                        required: "Email is required",
+                        pattern: {
+                          value:
+                            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                          message: "Invalid Email",
+                        },
+                      })}
+                    />
+                    {errors.email && (
+                      <p className="errors">{errors.email.message}</p>
+                    )}
+                  </div>
+                </div>
+                <div className="mb-3 row justify-content-center">
+                  <div className="col-sm-12">
+                    <input
+                      name="password"
+                      placeholder="Password"
+                      className="form-control shadow-none"
+                      type="password"
+                      {...register("password", {
+                        required: "Password is required",
+                      })}
+                    />
+                    {errors.password && (
+                      <p className="errors">{errors.password.message}</p>
+                    )}
+                  </div>
+                </div>
+                <div className="mt-4 mb-4 row justify-content-center">
+                  <div className="col-sm-12">
+                    <button type="submit" className="btn btn-auth">
+                      Sign In
+                    </button>
+                  </div>
+                </div>
+
+                <p>
+                  Forgot Password?{" "}
+                  <a href={Path.forgotPassword} className="link">
+                    Click Here
+                  </a>
+                </p>
+              </form>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <ToastContainer
+        position="bottom-center"
+        autoClose={5000}
+        hideProgressBar
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss={false}
+        draggable={false}
+        pauseOnHover
+        limit={1}
+        transition={Flip}
+      />
+    </>
   );
 };
 
