d3fbfb2eeb5a53afc17d1c9f0f803a0467ba4e5a
Mohammad Ahmed <mohd.ahmed379@gmail.com>
2021-11-30 17:54:48 +0530

    dashboard changes

--- src/Components/Common/Table/TagsTable/TagsTable.js
+++ src/Components/Common/Table/TagsTable/TagsTable.js
@@ -8,13 +8,13 @@ import paginationFactory, {
 } from "react-bootstrap-table2-paginator";
 import "react-bootstrap-table-next/dist/react-bootstrap-table2.min.css";
 import { TagsData } from "./TagsData";
-import {ReactComponent as EditIcon} from '../../../../Assets/Icon/Edit.svg';
-import {ReactComponent as DeleteIcon} from '../../../../Assets/Icon/Delete.svg';
+import { ReactComponent as EditIcon } from "../../../../Assets/Icon/Edit.svg";
+import { ReactComponent as DeleteIcon } from "../../../../Assets/Icon/Delete.svg";
 import "./Tags.css";
-import { confirmAlert } from 'react-confirm-alert';
-import {ReactComponent as BagIcon} from '../../../../Assets/Icon/Shoppingbasket.svg';
-import {ReactComponent as AddIcon} from '../../../../Assets/Icon/Add.svg';
-import 'react-confirm-alert/src/react-confirm-alert.css';
+import { confirmAlert } from "react-confirm-alert";
+import { ReactComponent as BagIcon } from "../../../../Assets/Icon/Shoppingbasket.svg";
+import { ReactComponent as AddIcon } from "../../../../Assets/Icon/Add.svg";
+import "react-confirm-alert/src/react-confirm-alert.css";
 import { useForm } from "react-hook-form";
 
 const TagsTable = () => {
@@ -35,41 +35,40 @@ const TagsTable = () => {
   const { SearchBar } = Search;
   const headerSortingStyle = { backgroundColor: "#e3edf8" };
 
-// function handleEdit(id, cat) {
-//   console.log(id)
-//   cat = category;
-//   console.log(cat);
-// }
+  // function handleEdit(id, cat) {
+  //   console.log(id)
+  //   cat = category;
+  //   console.log(cat);
+  // }
 
-// function addCategory()
-// {
-//   alert(true);
-//   setcatcheck(true);
-// }
+  // function addCategory()
+  // {
+  //   alert(true);
+  //   setcatcheck(true);
+  // }
 
-function settag(data)
-{
-  setTag(data.tags);
-  console.log(tag);
-}
+  function settag(data) {
+    setTag(data.tags);
+    console.log(tag);
+  }
 
   function handleDelete(rowId, name) {
     confirmAlert({
-      title: 'Delete',
+      title: "Delete",
       message: `Are you sure you want to remove this item from the table?`,
       buttons: [
         {
-          label: 'Yes',
-          className: 'btn btn-danger',
+          label: "Yes",
+          className: "btn btn-danger",
           onClick: () => {
-            console.log('ROW ID: ',rowId);
-          }
+            console.log("ROW ID: ", rowId);
+          },
         },
         {
-          label: 'No',
+          label: "No",
           // onClick: () => alert('Click No')
-        }
-      ]
+        },
+      ],
     });
   }
   const columns = [
@@ -97,25 +96,24 @@ function settag(data)
       headerAlign: "center",
       align: "center",
     },
-   
-      {
-        dataField: "link",
-        text: "Action",
-        headerAlign: "center",
-        align: "center",
-        formatter: (rowContent, row) => {
-          return (
-            <div className="d-flex justify-content-evenly">
-              
-                <EditIcon className="mt-1" />
-                <DeleteIcon className="iconHover" onClick={() => handleDelete(row.serialno, row.tags)}/>
-            
-            </div>
-          );
-        },
-      },
 
-   
+    {
+      dataField: "link",
+      text: "Action",
+      headerAlign: "center",
+      align: "center",
+      formatter: (rowContent, row) => {
+        return (
+          <div className="d-flex justify-content-evenly">
+            <EditIcon className="mt-1" />
+            <DeleteIcon
+              className="iconHover"
+              onClick={() => handleDelete(row.serialno, row.tags)}
+            />
+          </div>
+        );
+      },
+    },
   ];
 
   const defaultSorted = [
@@ -127,118 +125,145 @@ function settag(data)
 
   return (
     <>
-<div className="page-heading d-flex align-items-center p-3 justify-content-between">
-  <div className="d-flex pb-2 align-items-center">
-        <BagIcon
-          style={{ height: "36px", width: "36px", marginRight: "10px" }}
-        />
-        <h3 className="m-1">Tags </h3>
+      <div className="page-heading d-flex align-items-center p-4 justify-content-between">
+        <div className="page-heading-wapper d-flex">
+          <BagIcon className="page-icon m-0" />
+          <h3 className="page-sec-heading m-0 ms-2">Tags </h3>
         </div>
-        <div className="d-flex align-items-center "><button type="submit" className="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#exampleModal1"> <AddIcon /> Add New Tags</button></div>
+        <div className="d-flex align-items-center ">
+          <button
+            type="submit"
+            className="btn btn-secondary"
+            data-bs-toggle="modal"
+            data-bs-target="#exampleModal1"
+          >
+            {" "}
+            <AddIcon /> Add New Tags
+          </button>
+        </div>
       </div>
-<div className="modal fade" id="exampleModal1" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div className="modal-dialog modal-dialog-centered">
-    <div className="modal-content">
-    <div className="modal-header border-0">
-        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div className="modal-body p-4 pt-0">
-        <form onSubmit={handleSubmit(settag)}>
-          <div className="mb-3">
-            <label htmlFor="recipient-name" className="col-form-label">Tags Name</label>
-             <input type="text" className="form-control shadow-none" id="tags" placeholder="Tags" name="tags" autoComplete="off"
+      <div
+        className="modal fade"
+        id="exampleModal1"
+        tabIndex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog modal-dialog-centered">
+          <div className="modal-content">
+            <div className="modal-header border-0">
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body p-4 pt-0">
+              <form onSubmit={handleSubmit(settag)}>
+                <div className="mb-3">
+                  <label htmlFor="recipient-name" className="col-form-label">
+                    Tags Name
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control shadow-none"
+                    id="tags"
+                    placeholder="Tags"
+                    name="tags"
+                    autoComplete="off"
                     {...register("tags", {
                       required: "Tag is required",
-                    })}/> 
-                     {errors.tags && (
+                    })}
+                  />
+                  {errors.tags && (
                     <p className="errors">{errors.tags.message}</p>
                   )}
+                </div>
+                <div className="modal-footer border-0 d-flex justify-content-center">
+                  <button type="submit" className="btn btn-primary">
+                    Add Tags
+                  </button>
+                </div>
+              </form>
+            </div>
           </div>
-          <div className="modal-footer border-0 d-flex justify-content-center">
-        <button type="submit" className="btn btn-primary">Add Tags</button>
+        </div>
       </div>
-        </form>
-      </div>
-      
-    </div>
-  </div>
-</div>
 
+      <div className="card">
+        <div className="table-responsive" style={{ padding: "20px" }}>
+          <PaginationProvider
+            pagination={paginationFactory({
+              custom: true,
+              totalSize: products.length,
+              prePageText: "Previous",
+              nextPageText: "Next",
+              page: 1,
+              sizePerPage: 4,
+              sizePerPageList: [
+                {
+                  text: "5",
+                  value: 5,
+                },
 
-    <div className="card">
-    <div className="table-responsive" style={{ padding: "20px" }}>
-      <PaginationProvider
-        pagination={paginationFactory({
-          custom: true,
-          totalSize: products.length,
-          prePageText: "Previous",
-          nextPageText: "Next",
-          page: 1,
-          sizePerPage: 4,
-          sizePerPageList: [
-            {
-              text: "5",
-              value: 5,
-            },
-
-            {
-              text: "10",
-              value: 10,
-            },
-            {
-              text: "30",
-              value: 30,
-            },
-            {
-              text: "50",
-              value: 50,
-            },
-            {
-              text: "All",
-              value: products.length,
-            },
-          ],
-          hideSizePerPage: products.length === 0,
-        })}
-        keyField="id"
-        columns={columns}
-        data={TagsData.map((item) => item)}
-      >
-        {({ paginationProps, paginationTableProps }) => (
-          <ToolkitProvider
+                {
+                  text: "10",
+                  value: 10,
+                },
+                {
+                  text: "30",
+                  value: 30,
+                },
+                {
+                  text: "50",
+                  value: 50,
+                },
+                {
+                  text: "All",
+                  value: products.length,
+                },
+              ],
+              hideSizePerPage: products.length === 0,
+            })}
             keyField="id"
             columns={columns}
             data={TagsData.map((item) => item)}
-            search
           >
-            {(toolkitprops) => (
-              <>
-                <div className="d-flex justify-content-between mb-3">
-                  <SizePerPageDropdownStandalone {...paginationProps} />
-                  <SearchBar {...toolkitprops.searchProps} srText=" " />
-                </div>
-                <BootstrapTable
-                  {...toolkitprops.baseProps}
-                  {...paginationTableProps}
-                  defaultSorted={defaultSorted}
-                  defaultSortDirection="asc"
-                  wrapperClasses="table-responsive"
-                  hover
-                  striped
-                  condensed={false}
-                  noDataIndication="No Data Is Available"
-                />
-                <div className="d-flex justify-content-end">
-                  <PaginationListStandalone {...paginationProps} />
-                </div>
-              </>
+            {({ paginationProps, paginationTableProps }) => (
+              <ToolkitProvider
+                keyField="id"
+                columns={columns}
+                data={TagsData.map((item) => item)}
+                search
+              >
+                {(toolkitprops) => (
+                  <>
+                    <div className="d-flex justify-content-between mb-3">
+                      <SizePerPageDropdownStandalone {...paginationProps} />
+                      <SearchBar {...toolkitprops.searchProps} srText=" " />
+                    </div>
+                    <BootstrapTable
+                      {...toolkitprops.baseProps}
+                      {...paginationTableProps}
+                      defaultSorted={defaultSorted}
+                      defaultSortDirection="asc"
+                      wrapperClasses="table-responsive"
+                      hover
+                      striped
+                      condensed={false}
+                      noDataIndication="No Data Is Available"
+                    />
+                    <div className="d-flex justify-content-end">
+                      <PaginationListStandalone {...paginationProps} />
+                    </div>
+                  </>
+                )}
+              </ToolkitProvider>
             )}
-          </ToolkitProvider>
-        )}
-      </PaginationProvider>
-      
-    </div>
-    </div>
+          </PaginationProvider>
+        </div>
+      </div>
     </>
   );
 };
