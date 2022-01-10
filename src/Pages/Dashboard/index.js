c5ca5806f11ab4f9501e18584a9cc2325237d970
Akshay80 <akshaybisht80@gmail.com>
2022-01-10 16:42:36 +0530

    change

--- src/Pages/Dashboard/index.js
+++ src/Pages/Dashboard/index.js
@@ -14,7 +14,7 @@ const Dashboard = () => {
         <div className="page-heading d-flex p-4">
           <div className="page-heading-wapper align-items-center d-flex">
             <Home className="page-icon m-0" />
-            <h3 className="page-sec-heading m-0 mx-2">Dashboard BROOOOOO </h3>
+            <h3 className="page-sec-heading m-0 mx-2">Dashboard BRO </h3>
           </div>
         </div>
         <div className="date-filters d-flex">
